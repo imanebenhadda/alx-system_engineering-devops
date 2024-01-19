e9b8fc098156c1a4567c088d211f691068f01a22
imanebenhadda <imanebenhaddapama@gmail.com>
2024-01-19 21:44:28 +0100

    install

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,28 +1,11 @@
-# install_flask.pp
-
-# Include the stdlib module for package management
-include stdlib
-
-# Define a class for installing Python and pip3
-class { 'python':
-  version => '3',
-}
-
-package { 'python3-pip':
-  ensure   => 'present',
-  provider => 'pip3',
-  require  => Class['python'],
-}
-
-# Install Flask version 2.1.0 and Werkzeug version 2.0.2 using pip3
-package { 'Flask':
+#!/usr/bin/puppet
+# Install Flask and Werkzeug
+package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
-  require  => Package['python3-pip'],
 }
 
-package { 'Werkzeug':
-  ensure   => '2.0.2',
+package { 'werkzeug':
+  ensure   => '2.1.1',
   provider => 'pip3',
-  require  => Package['python3-pip'],
 }
