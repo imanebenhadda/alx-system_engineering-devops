5930d7f1ed19862f9d4e2e7d4641bd217b3e8a6b
imanebenhadda <imanebenhaddapama@gmail.com>
2024-01-23 20:36:15 +0100

    transfer

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,22 +1,62 @@
-# Install nginx with puppet
-package { 'nginx':
-  ensure => 'installed',
+# Install and configure Nginx web server
+exec { 'apt-update':
+    command => '/usr/bin/apt-get update'
 }
 
+Exec['apt-update'] -> Package <| |>
+
+package { 'nginx':
+  ensure => installed,
+}
+
+# change folder rights
+exec { 'chmod www folder':
+  command => 'chmod -R 755 /var/www',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
 
 file { '/var/www/html/index.html':
-  ensure  => 'file',
+  ensure  => present,
   content => 'Hello World!',
-  mode    => '0644',
-  require => Package['nginx'],
+  path    => '/var/www/html/index.html',
 }
 
-exec { 'append_redirect_me':
-  command => "/usr/bin/sed -i '/^}$/i \\\n\tlocation \\/redirect_me {return 301 https:\\/\\/www.youtube.com\\/watch?v=QH2-TGUlwu4;}' /etc/nginx/sites-available/default",
+file { '/etc/nginx/sites-available/default':
+  ensure  => present,
+  path    => '/etc/nginx/sites-available/default',
+  content => 'server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+
+    root /var/www/html;
+    index index.html;
+
+    location /redirect_me {
+        rewrite ^ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;
+    }
+    error_page 404 /404.html;
+    location = /404.html {
+        internal;
+    }
+}',
+}
+
+file { '/var/www/html/404.html':
+  ensure  => present,
+  path    => '/var/www/html/404.html',
+  content => "Ceci n'est pas une page",
+}
+
+exec { 'ufw':
+  command  => "ufw allow 'Nginx HTTP'",
+  provider => 'shell',
+}
+exec { 'restart nginx service':
+  command => 'service nginx restart',
+  path    => '/usr/bin:/usr/sbin:/bin',
 }
 
 service { 'nginx':
-  ensure  => 'running',
-  enable  => true,
+  ensure  => running,
   require => Package['nginx'],
 }
--- 0x0C-web_server/README.md
+++ 0x0C-web_server/README.md
@@ -1,1 +1,4 @@
-readme
+# 0x0C-web_server Directory README
+
+## Web Server Directory
+This directory contains the configuration files and scripts related to the web server for our project. The web server serves as a crucial component in delivering web content, handling HTTP requests, and ensuring the accessibility of our application or website.
